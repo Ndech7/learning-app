8fac9acfe0acd8646a7ce0fbbe84bd145b9716b7
Ndech7 <stephennderitu3@gmail.com>
2023-10-05 11:26:07 +0300

    Update baseURL

--- src/http.common.ts
+++ src/http.common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://learning-app.onrender.com/api/hubs",
+  baseURL: "http://learning-app-api.onrender.com/api/hubs",
   headers: {
     "Content-Type": "application/json",
   },
